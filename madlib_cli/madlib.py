ab849ff572b42ca8dd8aa8911be643059837668e
Chloe Nott <chloe@nott.email>
2021-12-01 14:09:37 -0800

    Initial setup. test_read_template_returns_stripped_string test passes

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,26 @@
+
+
+
+
+def read_template(path):
+    with open(path) as file:
+        contents = file.read().strip()
+    print(contents)
+    return contents
+
+def parse_template():
+    pass
+
+def merge():
+    pass
+
+
+
+
+
+
+
+
+
+
+
