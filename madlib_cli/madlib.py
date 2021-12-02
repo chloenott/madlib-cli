1e61478953ce3bfde85afa4c1419a5f20d2de392
Chloe Nott <chloe@nott.email>
2021-12-01 17:37:37 -0800

    Added docstrings to functions. Added example output txt file. All tests pass. Script is fully functional.

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,7 +1,10 @@
 import re
 
+TEMPLATE_PATH = "assets/make_me_a_video_game_template.txt"
+MADLIB_OUTPUT_PATH = "assets/madlib_output.txt"
 
 def read_template(path):
+    """Accepts a string representing a file path as an input. Returns the contents of the file."""
     try:
         with open(path) as file:
             contents = file.read().strip()
@@ -9,22 +12,52 @@ def read_template(path):
     except:
         raise FileNotFoundError
 
+def write_template(path, text):
+    """Writes the passed in text to the passed in file path (string)"""
+    with open(path, 'w') as file:
+        contents = file.write(text)
 
 def parse_template(string):
-    regex = r"{(\w*)}"
+    """
+    Returns a tuple consisting of the parsed template (string) and parts (tuple of strings).
+
+    "A {Adjective} and {Adjective} {Noun}" -> ("A {} and {} {}", ("Adjective", "Adjective", "Noun")
+    """
+    regex = r"{([^{}]*)}"
     parts = tuple(re.findall(regex, string))
     stripped = re.sub(regex, '{}', string)
     return (stripped, parts)
 
+def merge(template, words):
+    """
+    ("A {} and {} {}", ("dark", "stormy", "night") -> "A dark and stormy night."
+    """
+    return template.format(*words)
 
-def merge(stripped, parts):
-    return stripped.format(*parts)
+# Print a welcome message to the user, explaining the Madlib process and command line interactions
+print("Welcome User! Enter in some words and we'll create a fictional masterpiece out of it for you!")
+
+# Read a template Madlib file (Example), and parse that file into usable parts.
+template = read_template(TEMPLATE_PATH)
+template_parsed, parts = parse_template(template)
+
+# Prompt the user to submit a series of words to fit each of the required components of the Madlib template.
+user_inputs = []
+for part in parts:
+    user_inputs.append(input(part + ': '))
+
+# With the collected user inputs, populate the template such that each provided input is placed into the correct position within the template.
+madlib = merge(template_parsed, user_inputs)
+
+# After the resulting Madlib has been completed, provide the completed response back to the user in the command line.
+print(madlib)
+
+# Write the completed text (Example)to a new file on your file system (in the repo).
+write_template(MADLIB_OUTPUT_PATH, madlib)
 
 
 
-parse_template(
-        "It was a {Adjective} and {Adjective} {Noun}."
-    )
+
 
 
 
